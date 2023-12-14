87f081f04ec8c6ceb12132536c4cacc8fb737d3f
prasaanth08 <153792200+prasaanth08@users.noreply.github.com>
2023-12-13 22:15:51 -0800

    Add files via upload

---
+++ database msql schema/laravel.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `laravel`
+ */
+
+/* `laravel`.`bookings` */
+$bookings = array(
+  array('id' => '1','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'appache','b_number' => 'tn72 b1234','date' => '2023-12-12','service_need' => 'oil change','status' => 'Ready to delevery','created_at' => '2023-12-11 11:16:19','updated_at' => '2023-12-13 08:50:19'),
+  array('id' => '2','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'RE 350','b_number' => 'Tn72b13','date' => '2094-12-11','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-12 10:16:20','updated_at' => '2023-12-12 10:34:14'),
+  array('id' => '3','name' => 'tharik','email' => 'tharik@gmail.com','p_number' => '0987654321','b_model' => 'RE 350','b_number' => 'tn72 b1234','date' => '5272-12-23','service_need' => 'genaral service','status' => 'completed','created_at' => '2023-12-12 10:45:06','updated_at' => '2023-12-13 17:01:56'),
+  array('id' => '4','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:07:16','updated_at' => '2023-12-13 17:01:05'),
+  array('id' => '5','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:08:38','updated_at' => '2023-12-13 16:59:44'),
+  array('id' => '6','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'pending','created_at' => '2023-12-13 05:16:25','updated_at' => '2023-12-13 05:16:25'),
+  array('id' => '7','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:39:19','updated_at' => '2023-12-13 16:55:28'),
+  array('id' => '8','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:44:13','updated_at' => '2023-12-13 14:08:30'),
+  array('id' => '9','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:45:14','updated_at' => '2023-12-13 14:12:07'),
+  array('id' => '10','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:45:41','updated_at' => '2023-12-13 14:11:39'),
+  array('id' => '11','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'ktm','b_number' => 'ktm','date' => '2023-12-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 05:56:23','updated_at' => '2023-12-13 14:11:13'),
+  array('id' => '12','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'xl','b_number' => 'tn72 b1234','date' => '2024-02-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 06:45:50','updated_at' => '2023-12-13 14:10:45'),
+  array('id' => '13','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'xl','b_number' => 'tn72 b1234','date' => '2024-02-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 06:46:29','updated_at' => '2023-12-13 14:10:18'),
+  array('id' => '14','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'xl','b_number' => 'tn72 b1234','date' => '2024-02-13','service_need' => 'oil change','status' => 'completed','created_at' => '2023-12-13 06:47:41','updated_at' => '2023-12-13 14:09:41'),
+  array('id' => '15','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'hero','b_number' => 'tn72 b1234','date' => '2023-12-13','service_need' => 'model','status' => 'completed','created_at' => '2023-12-13 07:55:44','updated_at' => '2023-12-13 14:04:37'),
+  array('id' => '16','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'hero','b_number' => 'tn72 b1234','date' => '2023-12-13','service_need' => 'model','status' => 'completed','created_at' => '2023-12-13 07:56:30','updated_at' => '2023-12-13 14:03:51'),
+  array('id' => '17','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'rr310','b_number' => 'tn72 b1234','date' => '2023-12-13','service_need' => 'genaral service','status' => 'completed','created_at' => '2023-12-13 07:59:22','updated_at' => '2023-12-13 08:56:25'),
+  array('id' => '18','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','p_number' => '6383853263','b_model' => 'RE 350','b_number' => 'RE 350','date' => '2023-12-15','service_need' => 'genaral service','status' => 'pending','created_at' => '2023-12-14 03:33:04','updated_at' => '2023-12-14 03:33:04')
+);
+
+/* `laravel`.`failed_jobs` */
+$failed_jobs = array(
+);
+
+/* `laravel`.`migrations` */
+$migrations = array(
+  array('id' => '1','migration' => '2014_10_12_000000_create_users_table','batch' => '1'),
+  array('id' => '2','migration' => '2014_10_12_100000_create_password_reset_tokens_table','batch' => '1'),
+  array('id' => '3','migration' => '2019_08_19_000000_create_failed_jobs_table','batch' => '1'),
+  array('id' => '4','migration' => '2019_12_14_000001_create_personal_access_tokens_table','batch' => '1'),
+  array('id' => '5','migration' => '2023_12_09_103637_add_role_to_users','batch' => '2'),
+  array('id' => '6','migration' => '2023_12_09_141410_add_phone_to_users','batch' => '3'),
+  array('id' => '7','migration' => '2023_12_10_121716_create_services_table','batch' => '4'),
+  array('id' => '8','migration' => '2023_12_11_101909_create_bookings_table','batch' => '5'),
+  array('id' => '9','migration' => '2023_12_11_102703_create_bookings_table','batch' => '6'),
+  array('id' => '10','migration' => '2023_12_11_111209_add_status_to_bookings','batch' => '7')
+);
+
+/* `laravel`.`password_reset_tokens` */
+$password_reset_tokens = array(
+);
+
+/* `laravel`.`personal_access_tokens` */
+$personal_access_tokens = array(
+);
+
+/* `laravel`.`services` */
+$services = array(
+  array('id' => '5','topic' => 'oil change','pic' => '1702495546.jpg','description' => 'good','created_at' => '2023-12-11 09:20:02','updated_at' => '2023-12-13 19:25:46'),
+  array('id' => '7','topic' => 'genaral service','pic' => '1702356568.png','description' => 'SADMHFKashg<','created_at' => '2023-12-12 04:49:28','updated_at' => '2023-12-12 04:49:28'),
+  array('id' => '8','topic' => 'water service','pic' => '1702471275.png','description' => 'odsufyoiddl','created_at' => '2023-12-13 12:41:15','updated_at' => '2023-12-13 12:41:15'),
+  array('id' => '9','topic' => 'Normal checkup','pic' => '1702524095.png','description' => 'good','created_at' => '2023-12-14 03:21:35','updated_at' => '2023-12-14 03:21:35')
+);
+
+/* `laravel`.`users` */
+$users = array(
+  array('id' => '1','name' => 'Admin','email' => 'admin@gmail.com','phone' => '1234567890','email_verified_at' => NULL,'password' => '$2y$12$BJyzfcvPSraPUOV7MkhMmu3qj2nwQgml1DM8NGSxxNw72DKzbXzLm','remember_token' => NULL,'created_at' => NULL,'updated_at' => NULL,'role' => '0'),
+  array('id' => '2','name' => 'Prasaanth.s','email' => 'prasaanth.se@gmail.com','phone' => '6383853263','email_verified_at' => NULL,'password' => '$2y$12$wpUDm2azrg/5PcpK/O4DUuC8A4NQJ9Ji6TjX25dVGOH0lUN.AToXe','remember_token' => NULL,'created_at' => '2023-12-09 14:38:30','updated_at' => '2023-12-09 14:38:30','role' => '1'),
+  array('id' => '3','name' => 'tharik','email' => 'tharik@gmail.com','phone' => '0987654321','email_verified_at' => NULL,'password' => '$2y$12$38CbpWZPLtPs/kFJeT9WlOYpsQL1XjiTcbA/wVanOEHRztQDU09gq','remember_token' => NULL,'created_at' => '2023-12-12 10:43:45','updated_at' => '2023-12-12 10:43:45','role' => '1')
+);
